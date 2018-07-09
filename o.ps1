6d1ceb0e242133bc12527133526c943a1bbec009
RJK-Engineering <rjklinkhamer@gmail.com>
2018-07-09 11:30:50 +0200

    Uncommented -ExportManifest

--- o.ps1
+++ o.ps1
@@ -5,7 +5,7 @@ $settings = @{
     DataSetDir = "C:\Programs\IBM\FileNet\ContentEngine\tools\deploy\P8DeploymentData\Environments\Ontwikkel\Assets"
     SourceEnvironment = "Ontwikkel"
     # ExportManifest = "C:\Programs\IBM\FileNet\ContentEngine\tools\deploy\P8DeploymentData\Environments\Ontwikkel\Assets\ExportManifests\123test.xml"
-    ExportManifest = "P:\dmtool\123test.xml"
+    # ExportManifest = "P:\dmtool\123test.xml"
 }
 
 $dir = (Get-Item $PSCommandPath).Directory.FullName
