779c4ed84cef13d236bcf13cfef63eb6f640f5d2
RJK-Engineering <rjklinkhamer@gmail.com>
2018-07-10 15:19:22 +0200

    Update

--- o.ps1
+++ o.ps1
@@ -1,11 +1,8 @@
 $settings = @{
     PackageDir = "P:\packages"
     TemplateDir = "P:\dmtool\Templates"
-    # OptionSetPath = "P:\packages\DM_Option_Set.xml"
     DataSetDir = "C:\Programs\IBM\FileNet\ContentEngine\tools\deploy\P8DeploymentData\Environments\Ontwikkel\Assets"
     SourceEnvironment = "Ontwikkel"
-    # ExportManifest = "C:\Programs\IBM\FileNet\ContentEngine\tools\deploy\P8DeploymentData\Environments\Ontwikkel\Assets\ExportManifests\123test.xml"
-    # ExportManifest = "P:\dmtool\123test.xml"
 }
 
 $dir = (Get-Item $PSCommandPath).Directory.FullName
--- t.ps1
+++ t.ps1
@@ -1,10 +1,10 @@
 $settings = @{
     PackageDir = "P:\packages"
     TemplateDir = "P:\dmtool\Templates"
-    # OptionSetPath = "P:\packages\DM_Option_Set.xml"
     DataSetDir = "C:\Programs\IBM\FileNet\ContentEngine\tools\deploy\P8DeploymentData\Environments\kiwi Ontwikkel (Nieuwe)\Assets"
     ConvertedDataSetDir = "C:\Programs\IBM\FileNet\ContentEngine\tools\deploy\P8DeploymentData\Environments\kiwi Test\Assets"
     SourceEnvironment = "kiwi Ontwikkel (Nieuwe)"
+    TargetEnvironment = "kiwi Test"
     Pair = "Ontwikkel (Nieuwe) - Test"
 }
 
