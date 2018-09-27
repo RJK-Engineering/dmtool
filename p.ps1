e623e4eb22945679c466b3dd7cc34fffb1af2aef
RJK-Engineering <rjklinkhamer@gmail.com>
2018-09-27 12:31:21 +0200

    Corrected paths

--- p.ps1
+++ p.ps1
@@ -1,11 +1,11 @@
 $settings = @{
     TemplateDir = "$PSScriptRoot\Templates"
     DeploymentTree = "C:\Programs\IBM\FileNet\ContentEngine\tools\deploy\P8DeploymentData"
-    DataSetDir = "Environments\kiwi Ontwikkel (Nieuwe)\Assets"
-    ConvertedDataSetDir = "Environments\kiwi Productie\Assets"
-    SourceEnvironment = "kiwi Ontwikkel (Nieuwe)"
-    DestinationEnvironment = "kiwi Productie"
-    Pair = "Ontwikkel (Nieuwe) - Test"
+    DataSetDir = "Environments\kiwi Test\Assets"
+    ConvertedDataSetDir = "Environments\PROD\Assets"
+    SourceEnvironment = "kiwi Test"
+    DestinationEnvironment = "PROD"
+    Pair = "Test to Prod"
 }
 
 & "$PSScriptRoot\dmtool.ps1" @args @settings
