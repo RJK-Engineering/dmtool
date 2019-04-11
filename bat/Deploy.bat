c42f254d6ff46205ff50400804274c4ec38b1fa9
RJK-Engineering <rjklinkhamer@gmail.com>
2019-04-11 11:50:10 +0200

    Added checks and help

--- bat/Deploy.bat
+++ bat/Deploy.bat
@@ -1,14 +1,27 @@
 @echo off
 
-set PASSWORD=%1
-IF "%PASSWORD%"=="" GOTO NOPASS
+echo.%*|findstr/I/C:"-password" >nul 2>&1 || goto NOPASSWORD
+
+IF "%DMTOOL%"=="" GOTO HELP
+IF NOT EXIST "%DMTOOL%" GOTO DNE
+IF "%SOURCE%"=="" GOTO HELP
+IF "%DESTINATION%"=="" GOTO HELP
+IF "%PAIR%"=="" GOTO HELP
 
 set ENVOPTS=-SourceEnvironment '%SOURCE%' -DestinationEnvironment '%DESTINATION%' -Pair '%PAIR%'
-PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& %DMTOOL%\dmtool.ps1 -Build  %ENVOPTS% -Log %2 %3 %4"
-PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& %DMTOOL%\dmtool.ps1 -Deploy %ENVOPTS% -Log -Password '%PASSWORD%' %2 %3 %4"
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& %DMTOOL%\dmtool.ps1 -Deploy %ENVOPTS% -Log %*"
 goto END
 
-:NOPASS
-echo Geen password opgegeven
+:NOPASSWORD
+echo Geen password opgegeven, gebruik: -password [geheimwachtwoord]
+goto END
+
+:HELP
+echo De volgende omgevingsvariabelen moeten gedefinieerd zijn: DMTOOL SOURCE DESTINATION PAIR
+goto END
+
+:DNE
+echo Pad bestaat niet: set DMTOOL=%DMTOOL%
+goto END
 
 :END
